bab86bb5f9164624b89adaf2e766541ff7d3f92d
Dennis Jackson <dennisdjack1987@gmail.com>
2023-08-07 23:49:58 -0700

    fixing commits

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import express from  'express'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 
 
@@ -73,7 +79,7 @@ export const runWithEmbeddings = async (question) => {
 
 
 
-
+app.use(express.static(path.join(__dirname+'/public')))
 app.post('/api/v1/handbook',async(req,res)=>{
 
     const {question} =req.body
